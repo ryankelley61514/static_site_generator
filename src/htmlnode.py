4057f1716fc3564da24b8326bd51f3f8a40bbc87
Ryan <ryan.kelley615@gmail.com>
2025-11-27 14:23:57 -0500

    LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,14 @@ class HTMLNode():
             props_string += f' {key}="{value}"'
         
         return props_string
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode has no value.")
+        if not self.tag:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,22 +1,31 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
-        node = HTMLNode("a", "hello world", None, {"href": "https://boot.dev", "target": "_blank"})
-        expected = "HTMLNode(\ntag: a\nvalue: hello world\nchildren: None\nprops: {'href': 'https://boot.dev', 'target': '_blank'}\n)"
+        node = HTMLNode("a", "Boot.dev", None, {"href": "https://boot.dev", "target": "_blank"})
+        expected = "HTMLNode(\ntag: a\nvalue: Boot.dev\nchildren: None\nprops: {'href': 'https://boot.dev', 'target': '_blank'}\n)"
         self.assertEqual(node.__repr__(), expected)
     
     def test_props_to_html(self):
-        node = HTMLNode("a", "hello world", None, {"href": "https://boot.dev", "target": "_blank"})
+        node = HTMLNode("a", "Boot.dev", None, {"href": "https://boot.dev", "target": "_blank"})
         expected = ' href="https://boot.dev" target="_blank"'
         self.assertEqual(node.props_to_html(), expected)
     
     def test_to_html(self):
-        node = HTMLNode("a", "hello world", None, {"href": "https://boot.dev", "target": "_blank"})
+        node = HTMLNode("a", "Boot.dev", None, {"href": "https://boot.dev", "target": "_blank"})
         with self.assertRaises(NotImplementedError):
             node.to_html()
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Boot.dev", {"href": "https://boot.dev", "target": "_blank"})
+        expected = '<a href="https://boot.dev" target="_blank">Boot.dev</a>'
+        self.assertEqual(node.to_html(), expected)
 
 if __name__ == "__main__":
     unittest.main()
